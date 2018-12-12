6a40df286f37839c1af437eb0be2c82f6d3c14a4
thomasfny <36214258+thomasfny@users.noreply.github.com>
2018-12-12 12:23:50 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -3,22 +3,17 @@ const https = require('https');
 const fs = require('fs');
 const url = require('url');
 const querystring=require('querystring');
-//const JSON = require('json');
 const credentials_json=fs.readFileSync('./auth/credentials.json','utf-8');
 const credentials=JSON.parse(credentials_json);
 
 const post_data=querystring.stringify(credentials);
-//post_data.concat("&grant_type=client_credentials");
 let options ={'method' :  "POST" , 'headers' : {  'Content-Type' : 'application/x-www-form-urlencoded' ,  'Content-Length' : post_data.length}}
 const server_address = 'localhost';
 const port = 3000;
+
 let html_stream = fs.createReadStream('./html/search-form.html','utf8');
 let image_stream= fs.readFileSync('./artist/pig.jpeg');
-const authentication_req_url='https://accounts.spotify.com/api/token';
-		
-	let authentication_req=https.request(authentication_req_url,options,authentication_res=>{
-		received_authentication(authentication_res,res,user_input,request_sent_time);
-	});
+
 let server = http.createServer((req,res)=>{
 	let booler;
 	if(req.url==='/'){
@@ -27,9 +22,8 @@ let server = http.createServer((req,res)=>{
 		html_stream.pipe(res);
 	}
 	else if(req.url==='/favicon.ico'){
-		//console.log(`A new request was made from ${req.connection.remoteAddress} : ${req.url}`);
 		res.writeHead(200,{'Content-Type':'text/html'});		
-	}	
+		}	
 	else {		
 		let request_sent_time=Date.now();
 		console.log(`A new request was made from ${req.connection.remoteAddress} : ${req.url}`);
@@ -37,9 +31,9 @@ let server = http.createServer((req,res)=>{
 		let user_input=req.url.substring(15,req.url.length);
 		console.log(user_input);
 		fs.writeFileSync('./auth/a.json',JSON.stringify(user_input), function(err){
-		if(err){return console.log(err);}
-	});
-		console.log(post_data);
+			if(err){return console.log(err);}		
+		});
+		console.log(post_data);				
 		let cache_valid=false;		
 		if(fs.existsSync('./auth/authentication_res.json')){
 			let content=fs.readFileSync('./auth/authentication_res.json','utf-8');
@@ -51,33 +45,33 @@ let server = http.createServer((req,res)=>{
 			}
 			else{
 				console.log('Token Expired');
-			}
-		}
+			}			
+		}		
 		if(cache_valid){
 			let content=fs.readFileSync('./auth/authentication_res.json','utf-8');
 			let cache_auth=JSON.parse(content);
 			setTimeout(() => { create_serch_req(cache_auth,user_input,request_sent_time)},10);			
 		}
-		else{			
-		// const authentication_req_url='https://accounts.spotify.com/api/token';		
-		// let authentication_req=https.request(authentication_req_url,options,authentication_res=>{
-			// received_authentication(authentication_res,res,user_input,request_sent_time);
-		// });
-		authentication_req.on('error',(e)=>{
-			console.error(e);
-		});
-		authentication_req.write(post_data);
-		console.log("Requesting Token");
-		authentication_req.end();
-		let content=fs.readFileSync('./auth/authentication_res.json','utf-8');
-		let cache_auth=JSON.parse(content);
-		setTimeout(() => { create_serch_req(cache_auth,user_input,request_sent_time)},10);				
+		else{					
+			const authentication_req_url='https://accounts.spotify.com/api/token';			
+			let authentication_req=https.request(authentication_req_url,options,authentication_res=>{
+				received_authentication(authentication_res,res,user_input,request_sent_time);
+			});
+			authentication_req.on('error',(e)=>{
+				console.error(e);
+			});
+			authentication_req.write(post_data);
+			console.log("Requesting Token");
+			authentication_req.end();
+			let content=fs.readFileSync('./auth/authentication_res.json','utf-8');
+			let cache_auth=JSON.parse(content);
+			setTimeout(() => { create_serch_req(cache_auth,user_input,request_sent_time)},10);				
 		}		
 		res.end(image_stream, 'binary');
-	}
+	}	 
 });
-
-function create_cache(authentication_res_data,user_input){			
+function create_cache(authentication_res_data,user_input){		
+	
 	authentication_res_data.user_input=user_input;
 	fs.writeFileSync('./auth/authentication_res.json',JSON.stringify(authentication_res_data), function(err){
 		if(err){return console.log(err);}
@@ -94,7 +88,7 @@ function create_serch_req(authentication_res_data,user_input,request_sent_time){
 	let user_options ={'method' :  "GET" , 'headers' : { 'Authorization' : 'Bearer '+user_credential.access_token   }}
 	//let user_options ={'method' :  "GET", ' };
 	let user_req_url='https://api.spotify.com/v1/search?';
-	console.log(user_credential.user_input);
+	//console.log('$$$$$$$$$$$$$$$$$$$$$'+user_credential.user_input);
 	user_req_url+=querystring.stringify({ q: user_search_j, type: 'artist' });
 	console.log(user_req_url);
 	user_result=https.request(user_req_url,user_options,res=>{
@@ -102,30 +96,32 @@ function create_serch_req(authentication_res_data,user_input,request_sent_time){
 		let body="";
 		res.on("data",data=> {body+=data;});
 		res.on("end", ()=> {
-		
-		console.log(body);
-		let img_path='./artist/'+user_search_j+'.jpeg';
-		let artists=body["items"];
-		console.log(artists);
-		let items=artists["items"];
-		let image_s=items[0].images;
-		let image_req=https.get(image_s[0].url,image_res => {
-		let new_img=fs.createWriteStream(img_path,{ 'encoding':null});
-		image_res.pipe(new_img);
-		new_img.on('finish',function(){
-			//console.log(body.artists.images[0].url);
-		});
-	});
-		image_req.on('error',function(err){console.log(err);});
+		const seaObj=JSON.parse(body);
+		if(seaObj.artist.items.length==0){
+			return res.end('No artist found.');
+		}
+		const artisInfo={name:seaObj.artists.
+		// console.log(body);
+		// let img_path='./artist/'+user_search_j+'.jpeg';
+		// console.log(img_path);
+		// let artists=body["items"];
+		//console.log(artists);
+		// let items=artists["items"];
+		// let image_s=items[0].images;
+		// let image_req=https.get(image_s[0].url,image_res => {
+		// let new_img=fs.createWriteStream(img_path,{ 'encoding':null});
+		// image_res.pipe(new_img);
+		// new_img.on('finish',function(){
+		// });
+	// });
+		// image_req.on('error',function(err){console.log(err);});			
 	});
 	});
 	user_result.on('error',(e)=>{
 			console.error(e);
 	});
-	//user_result.write(user_token_q);
 	console.log("User Request");
 	user_result.end();
-	//console.log(user_result);
 }
 function received_authentication(authentication_res,res,user_input,request_sent_time){
 	authentication_res.setEncoding("utf8");
@@ -138,9 +134,10 @@ function received_authentication(authentication_res,res,user_input,request_sent_
 		let temp=request_sent_time;
 		let tem=new Date(temp);	
 		tem.setHours(tem.getHours()+1);
-		authentication_res_data.expiration= tem;		
-		console.log(authentication_res_data.expiration);			
-		create_cache(authentication_res_data,user_input);		
+		authentication_res_data.expiration= tem.toString();		
+		console.log(authentication_res_data.expiration);				
+		create_cache(authentication_res_data,user_input);
+		
 	});	
 }
 console.log('Now listening on port ' + port);
--- new.js
+++ new.js
@@ -26,13 +26,13 @@ let server = http.createServer((req,res)=>{
 		}
 	else {
 		let request_sent_time=Date.now();
+		console.log(Date(request_sent_time));
 		console.log(`A new request was made from ${req.connection.remoteAddress} : ${req.url}`);
 		res.writeHead(200,{'Content-Type':'image/jpeg'});
 		let user_input=req.url.substring(15,req.url.length);
 		console.log(user_input);
 		fs.writeFileSync('./auth/a.json',JSON.stringify(user_input), function(err){
-		if(err){return console.log(err);
-		}		
+		if(err){return console.log(err);}		
 		});
 		console.log(post_data);
 		const authentication_req_url='https://accounts.spotify.com/api/token';		
@@ -41,9 +41,26 @@ let server = http.createServer((req,res)=>{
 		});
 		authentication_req.on('error',(e)=>{
 			console.error(e);
-		});
-		authentication_req.write(post_data);
+		});		 
+		//
+		let cache_valid=false;
+		if(fs.existsSync('./auth/authentication_res.json')){
+			let content=fs.readFileSync('./auth/authentication_res.json','utf-8');			
+			let cache_auth=JSON.parse(content);
+			booler=cache_auth;
+			console.log("hello"+new Date(booler.expiration));
+			if(new Date(cache_auth.expiration)>Date.now()){
+				cache_valid=true;
+				console.log(cache_valid);
+			}
+			else{
+				console.log('Token Expired');
+			}
+		}
+		//		
+		//authentication_req.write(post_data);
 		console.log("Requesting Token");
+		console.log(authentication_req);
 		authentication_req.end();		
 		res.end(image_stream, 'binary');
 	}	
@@ -64,6 +81,14 @@ function received_authentication(authentication_res,res,user_input,request_sent_
 	authentication_res.on("end", ()=> {
 		let authentication_res_data=JSON.parse(body);
 		console.log(authentication_res_data);
+		//
+		console.log(request_sent_time);
+		let temp=request_sent_time;
+		let tem=new Date(temp);	
+		tem.setHours(tem.getHours()+1);
+		authentication_res_data.expiration= tem.toString();		
+		console.log(authentication_res_data.expiration);	
+		//		
 		create_cache(authentication_res_data,user_input);
 		//create_serch_req(authentication_res_data,res,user_input,request_sent_time);
 	});
